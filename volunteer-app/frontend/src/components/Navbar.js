d6038d50481b6daf49d5bb7fbbf2fadda1b74e5f
GlowSand <78033979+GlowSand@users.noreply.github.com>
2025-09-25 19:02:19 -0500

    Revert "Merge volunteer-app into 4353-Project under volunteer-app folder"
    This reverts commit 6444b67ddbe155898cf5b2d9e8730e90e0e5d63e.

--- volunteer-app/frontend/src/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Navbar() {
-  return (
-    <nav style={{ padding: 12, borderBottom: '1px solid #ddd' }}>
-      <h1 style={{ margin: 0 }}>Volunteer App</h1>
-    </nav>
-  );
-}
